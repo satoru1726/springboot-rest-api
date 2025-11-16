3fb2c5dc5e52fba56bd9aa0d2f99196b71d65be4
Jayanth A R <127710029+satoru1726@users.noreply.github.com>
2025-11-17 00:38:11 +0530

    updated ProductRepo.java

--- src/main/java/in/elmarino/springWebApp/repositories/ProductRepo.java
+++ src/main/java/in/elmarino/springWebApp/repositories/ProductRepo.java
@@ -4,10 +4,7 @@ import in.elmarino.springWebApp.model.ProductClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface ProductRepo extends JpaRepository<ProductClass, Integer> {
-    // <ModelClass, type of your primary key>
-
-
+    
 }
